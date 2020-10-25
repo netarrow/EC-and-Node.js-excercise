20bda688469c5fe3d84117716c5c79b7da4899d2
Matteo <>
2020-10-25 14:46:29 +0100

    added hash calculation on file name. added html src fixer to ref scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,48 @@ var gulp = require('gulp');
 var del = require('del')
 var rename = require("gulp-rename");
 var webpack = require('webpack-stream');
+var hash = require('gulp-hash')
+var inject = require('gulp-inject-string');
+var tap = require('gulp-tap')
 var config = require('./webpack.config')
+var path = require('path')
 
-function clean() {
-    return del('dist/**/*', {force:true});
+var modernFile
+var legacyFile
+
+function clean(next) {
+    return del('dist/**/*', {force:true})
 }
 
-function modernPack() {
-    return gulp.src('src/js/index.js')
+function modernPack(next) {
+    gulp.src('src/js/index.js')
         .pipe(webpack(config.modern))
         .pipe(rename('bundle.js'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(hash())
+        .pipe(tap(file => modernFile = path.basename(file.path)))
+        .pipe(gulp.dest('dist/'))
+        .on('end', next)
 }
 
-function legacyPack() {
-    return gulp.src('src/js/index.js')
+function legacyPack(next) {
+    gulp.src('src/js/index.js')
         .pipe(webpack(config.legacy))
         .pipe(rename('bundle.legacy.js'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(hash())
+        .pipe(tap(file => legacyFile = path.basename(file.path)))
+        .pipe(gulp.dest('dist/'))
+        .on('end', next)
+}
+
+function copyHtmlWithFixedSrc(next) {
+    gulp.src('src/index.html')
+        .pipe(inject.replace('modernFile', modernFile))
+        .pipe(inject.replace('legacyFile', legacyFile))
+        .pipe(gulp.dest('dist/'))
+        .on('end', next)
 }
 
 gulp.task('build', function (done) {
     clean()
-    modernPack()
-    legacyPack()
-    return gulp.src('src/index.html').pipe(gulp.dest('dist/'))
+    modernPack(() => legacyPack(() => copyHtmlWithFixedSrc(() => done())))
 });
